5a9f13e7bd2b37155068037be45eeb4471b39f36
Tomas Gerulaitis <t.gerulaitis@gmail.com>
2011-08-20 16:54:24 +0100

    Added the ability to play files from the library.

--- library.js
+++ library.js
@@ -2,7 +2,6 @@ var library=[];
 
 /* Reads all of the files in a directory */
 function indexFiles(files){
-	var library = [];
 	var container = "#library";
 	
 	for (var i = 0; i < files.length; i++) {
@@ -10,7 +9,36 @@ function indexFiles(files){
 		
 		if (file.name.indexOf("mp3") != -1) {
 			library.push(file);
-			$(container).append($('<div>' + file.name + '</div>').addClass("row"));
 		}
+	}
+	
+	updateView();
+}
+
+/* Updates the view of the library */
+function updateView() {
+	var container = "#library";
+	var player = "#player";
+	
+	for (var i = 0; i < library.length; i++) {
+		var url;
+		if (window.createObjectURL) {
+			url = window.createObjectURL(library[i]);
+		} else if (window.createBlobURL) {
+			url = window.createBlobURL(library[i]);
+		} else if (window.URL && window.URL.createObjectURL) {
+			url = window.URL.createObjectURL(library[i]);
+		} else if (window.webkitURL && window.webkitURL.createObjectURL) {
+			url = window.webkitURL.createObjectURL(library[i]);
+		}
+		var link = $("<a></a>").text(library[i].name);
+		link.attr("href", url);
+		link.click(function () {
+			$(player).attr("src", this.href);
+			return false;
+		});
+		
+		$(container).append(link);
+		$(container).append($("<br/>"));
 	}
 }
