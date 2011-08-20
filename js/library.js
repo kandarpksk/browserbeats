dea1a13f9a26d1eddd44c57e1d1150d82e348788
Tomas Gerulaitis <t.gerulaitis@gmail.com>
2011-08-20 21:15:48 +0100

    Removed library caching and added ID3 support.

--- js/library.js
+++ js/library.js
@@ -8,81 +8,62 @@ function indexFiles(files){
 		var file = files[i];
 		
 		if (file.name.indexOf("mp3") != -1) {
-			library.push(file);
+			var entry = {};
+			// Get the blob url of the file
+			if (window.createObjectURL) {
+				entry.url = window.createObjectURL(file);
+			} else if (window.createBlobURL) {
+				entry.url = window.createBlobURL(file);
+			} else if (window.URL && window.URL.createObjectURL) {
+				entry.url = window.URL.createObjectURL(file);
+			} else if (window.webkitURL && window.webkitURL.createObjectURL) {
+				entry.url = window.webkitURL.createObjectURL(file);
+			}
+			// Get the tags
+			ID3.loadTags(entry.url, function() {
+				var tags = ID3.getAllTags(entry.url);
+				entry.artist = tags.artist;
+				entry.title = tags.title;
+				entry.album = tags.album;
+			},{dataReader: FileAPIReader(file)});
+			library.push(entry);
 		}
 	}
 	
-	saveLibrary();
 	updateView();
-}
-
-/* Check if the browser supports offline storage */
-function supportsLocalStorage() {
-  try {
-    return 'localStorage' in window && window['localStorage'] !== null;
-  } catch (e) {
-    return false;
-  }
-}
-
-/* Save the library so that we don't have to restore after each page refresh */
-function saveLibrary() {
-    if (!supportsLocalStorage()) {
-        // No native support for HTML 5 storage :(
-        alert("Upgrade your browser, bro.");
-        return false;
-    }
-    
-    console.log(JSON.stringify(library));
-    localStorage.setItem("browserbeats.library", JSON.stringify(library));
-}
-
-/* Load local library on page load if available */
-
-function loadLibrary() {
-    if (!supportsLocalStorage()) {
-        alert("Upgrade your browser, bro");
-        return false;
-    }
-    
-    var lib = JSON.parse(localStorage.getItem("browserbeats.library"));
-    if (lib != null ) {
-        library = lib;
-    }
-    
-    updateView();
 }
 
 /* Updates the view of the library */
 function updateView() {
-	var container = "#library";
-	var player = "#player";
+	var view = "#library";
+	
+	$(view).empty();
+	var header = $("<div></div>").addClass("row");
+	header.append($("<div></div>").addClass("span2 columns").text("Artist"));
+	header.append($("<div></div>").addClass("span2 columns").text("Title"));
+	header.append($("<div></div>").addClass("span2 columns").text("Album"));
+	$(view).append(header);
 	
 	for (var i = 0; i < library.length; i++) {
-		var url;
-		if (window.createObjectURL) {
-			url = window.createObjectURL(library[i]);
-		} else if (window.createBlobURL) {
-			url = window.createBlobURL(library[i]);
-		} else if (window.URL && window.URL.createObjectURL) {
-			url = window.URL.createObjectURL(library[i]);
-		} else if (window.webkitURL && window.webkitURL.createObjectURL) {
-			url = window.webkitURL.createObjectURL(library[i]);
-		}
-		var link = $("<a></a>").text(library[i].name);
-		link.attr("href", url);
-		link.click(function () {
-			$(player).attr("src", this.href);
-			return false;
-		});
+		var entry = library[i];
+		var container = $("<div></div>").addClass("row");
 		
-		$(container).append(link);
-		$(container).append($("<br/>"));
+		container.attr("id", entry.url);
+		
+		container.append($("<div></div>").addClass("span2 columns").text(entry.artist));
+		container.append($("<div></div>").addClass("span2 columns").text(entry.title));
+		container.append($("<div></div>").addClass("span2 columns").text(entry.album));
+		
+		$(view).append(container);
 	}
 }
 
-$(document).ready(function() {
-   loadLibrary(); 
-});
+/* Play a song */
+function play(){
+	var player = "#player";
+	
+	$(player).attr("src", this.id);
+	return false;
+}
 
 /* Don't edit below this line */
